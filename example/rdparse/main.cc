e9d58da3eb5c9dcf89081aa4f6bc52b3917090e7
pyxherb <codesbuilder@163.com>
2025-08-25 18:00:06 +0800

    Update main.

--- example/rdparse/main.cc
+++ example/rdparse/main.cc
@@ -2,16 +2,47 @@
 #include <peff/base/deallocable.h>
 #include <peff/advutils/unique_ptr.h>
 
+class HttpServer {
+
+};
+
 int main() {
+	netknot::ExceptionPointer e;
 	{
 		peff::UniquePtr<netknot::IOService, peff::DeallocableDeleter<netknot::IOService>> ioService;
+		{
+			netknot::IOServiceCreationParams params(peff::getDefaultAlloc(), peff::getDefaultAlloc());
 
-		netknot::IOServiceCreationParams params(peff::getDefaultAlloc(), peff::getDefaultAlloc());
+			if ((e = netknot::createDefaultIOService(ioService.getRef(), params))) {
+				std::terminate();
+			}
+		}
 
-		netknot::ExceptionPointer e = netknot::createDefaultIOService(ioService.getRef(), params);
+		peff::UniquePtr<netknot::CompiledAddress, peff::DeallocableDeleter<netknot::CompiledAddress>> compiledAddr;
+		{
+			netknot::IPv4Address addr(127, 0, 0, 0, 8080);
 
-		if (e) {
+			if ((e = ioService->compileAddress(peff::getDefaultAlloc(), &addr, compiledAddr.getRef()))) {
+				std::terminate();
+			}
+		}
+
+		peff::UniquePtr<netknot::Socket, peff::DeallocableDeleter<netknot::Socket>> socket;
+		{
+			if ((e = ioService->createSocket(peff::getDefaultAlloc(), netknot::ADDRFAM_IPV4, netknot::SOCKET_TCP, socket.getRef()))) {
+				std::terminate();
+			}
+		}
+
+		if ((e = socket->bind(compiledAddr.get()))) {
 			std::terminate();
 		}
+
+		peff::RcObjectPtr<netknot::AcceptAsyncTask> acceptAsyncTask;
+		if ((e = socket->acceptAsync(peff::getDefaultAlloc(), acceptAsyncTask.getRef()))) {
+			std::terminate();
+		}
+
+		ioService->run();
 	}
 }
